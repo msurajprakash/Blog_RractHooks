599d6a80f45defe3d01030a575020ff5a0d94ddb
msurajprakash <spmishra9319@gmail.com>
2023-03-20 00:39:13 +0530

    Added firebase to home and get the title and subtitle to home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,43 @@
+import { useState, useEffect } from "react";
+import { firestore } from '../firebase';
+import { NavLink } from "react-router-dom";
+
 function Home() {
-    return (
-      <div className="">
-        Home
-      </div>
-    );
-  }
-  
-  export default Home;
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    firestore
+      .collection('posts')
+      .get()
+      .then((snapshot) => {
+        const posts = snapshot.docs.map((doc) => {
+          return {
+            id: doc.id,
+            ...doc.data()
+          };
+        });
+
+        console.log('posts', posts);
+        setPosts(posts);
+      });
+  }, []);
+
+  return (
+    <div className="home">
+      <h1>Tech Blog</h1>
+      <div id="blog-by">Suraj</div>
+
+      {posts.map((post, index) => (
+        <div className="post" key={`post-${index}`}>
+          <NavLink to={`/post/${post.id}`}>
+            <h3>{post.title}</h3>
+          </NavLink>
+
+          <p>{post.subTitle}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Home;
